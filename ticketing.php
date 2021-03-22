17f93bc2e4311bed2346e486805ac48924ed97cd
Chiogros <32033845+Chiogros@users.noreply.github.com>
2021-03-22 19:14:10 +0100

    Add files via upload

--- ticketing.php
+++ ticketing.php
@@ -1,11 +1,6 @@
 <?php
-if(isset($_GET['date'])){
-	$date=htmlentities($_POST['date']);
-	$tickets=(int)htmlentities($_POST['number']);
-	$str;
-	
-}
-
+$cookie_date;
+$cookie_quantite;
 ?>
 
 <!DOCTYPE html>
@@ -21,56 +16,49 @@ if(isset($_GET['date'])){
 			<ul>
 				<li><a href="artworks.html">Artworks</a></li>
 				<li><a href="#">Ticketing</a></li>
-				<li><a href="#">Schedule</a></li>
 				<li><a href="contact.html">Contact us</a></li>
 			</ul>
 		</nav>
 		<hr>
 	</header>
-	<section>
-		<form method="GET" action="/ticketing.php">
-			<div>
-				<label for="date">Date : </label>
-  				<input type="date" id="date" name="date">
-			</div>
-			<div>
-				<label for="number">Quantity : </label>
-				<input type="number" id="number" name="number">
-			</div>
-			<input type="submit" value="Submit"><br>
-		</form>
-		<ul>
-			<h3>Payment method :</h3>
-			<li>idk</li>
-			<li>Dogecoin</li>
-		</ul>
-	</section>
-	<aside>
-		<?php
-			/*function basket() {
-				global $tickets;print "<p>salut</p>";
-				$tickets[$_POST['date']] = $tickets[$_POST['date']] + $_POST['number'];
-			}
+	<div>
+		<section>
+			<iframe src="https://calendar.google.com/calendar/embed?height=400&amp;wkst=2&amp;bgcolor=%23f55e61&amp;ctz=Europe%2FParis&amp;src=NDJjbGFhbzY3YWo1YmMyMzA1MnBqMWNwbDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23E4C441&amp;showDate=1&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;hl=en" style="border:solid 1px #777" width="800" height="400" frameborder="0" scrolling="no"></iframe>
+			<form method="GET" action="ticketing.php">
+				<div>
+					<label for="date">Date : </label>
+					<input type="date" id="date" name="date" required>
+				</div>
+				<div>
+					<label for="number">Quantity : </label>
+					<input type="number" id="number" name="number" required>
+				</div>
+				<input type="submit" value="Submit"><br>
+			</form>
+			<ul>
+				<h3>Payment method :</h3>
+				<li>idk</li>
+				<li>Dogecoin</li>
+			</ul>
+		</section>
+		<aside>
+			<?php
+				if (isset($_COOKIE[$_GET['date']])) {
+					$quantityAlreadyAdded = $_COOKIE[$_GET['date']];
+				}
+				setcookie($_GET['date'], $quantityAlreadyAdded + $_GET['number'], time() + (86400 *30), "/");
 
-			print '<table>';
-			global $tickets;
-			foreach($tickets as $date => $number) {
-				print "<tr><th>" . $date . "</th><td>" . $number . "</td></tr>"; 
-			}
-			print '</table>';
-			*/
-			if(isset($_GET['date'])){
-				echo "voila les daltons";
-				$str= "<table><tr><th>$date</th><td>$tickets</td></tr></table>";
-				echo $str;
-			}
-
-
-	
-
-
-		?>
-	</aside>
+				print '<table>';
+				if (count($_COOKIE) > 0) {
+					print "<h2>Cart</h2>";
+				}
+				foreach($_COOKIE as $date => $number) {
+					print "<tr><th>" . $date . "</th><td>" . $number . "</td></tr>"; 
+				}
+				print '</table>';
+			?>
+		</aside>
+	</div>
 	<footer>
 		<p>A London based Museum</p>
 	</footer>
